0a29cdcb6ed3f3eecb72d0dc9dd662ecbf535bfa
Adrian Wysocki <dobrystuff77@gmail.com>
2020-08-01 13:15:28 +0200

    fixing bugs

--- src/friendbutton.js
+++ src/friendbutton.js
@@ -86,7 +86,7 @@ export default function FriendButton(props) {
     return (
         <>
             <button
-                className="register-button"
+                // className="register-button"
                 style={{ marginTop: "10px" }}
                 onClick={unfriendlyButton}
             >
